e14cb03354ef33e03f698a67ab1187b57014b60e
DariiaKorovchenko <samurais.daughter007@gmail.com>
2025-03-09 21:47:07 +0200

    buy now modal

--- js/modal.js
+++ js/modal.js
@@ -13,3 +13,19 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-buynow-open]"),
+    closeModalBtn: document.querySelector("[data-modal-buynow-close]"),
+    modal: document.querySelector("[data-modal-buynow]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-buynow-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
