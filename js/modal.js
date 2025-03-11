b3b5e48f198aeff5c5aa5452c255918e5abfec32
DariiaKorovchenko <samurais.daughter007@gmail.com>
2025-03-11 22:13:06 +0200

    location modal, franchise modal, read more modal, refactoring

--- js/modal.js
+++ js/modal.js
@@ -29,3 +29,51 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-readmore-open]"),
+    closeModalBtn: document.querySelector("[data-modal-readmore-close]"),
+    modal: document.querySelector("[data-modal-readmore]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-readmore-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-location-open]"),
+    closeModalBtn: document.querySelector("[data-modal-location-close]"),
+    modal: document.querySelector("[data-modal-location]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-location-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-franchise-open]"),
+    closeModalBtn: document.querySelector("[data-modal-franchise-close]"),
+    modal: document.querySelector("[data-modal-franchise]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-franchise-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
